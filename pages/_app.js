45a2d343275f6ce94f4558001274708a3b7fa864
kattlover99 <thrillseeker.nw@gmail.com>
2023-11-19 11:08:15 -0500

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,18 @@ import '../styles/globals.css'
 import { MeshProvider } from "@meshsdk/react";
 // import { GameProvider } from '../context/GameProvider'
 import { UserProvider } from '../context/UserProvider'
+import { ErrorBoundary } from 'next/dist/client/components/error-boundary';
 // import { ModalProvider } from '../context/ModalProvider'
 function MyApp({ Component, pageProps }) {
   return (
-    <MeshProvider>
-      <UserProvider>
+    <ErrorBoundary>
+      <MeshProvider>
+        <UserProvider>
           <Component {...pageProps} />
           <ToastContainer />
-      </UserProvider>
-    </MeshProvider>
+        </UserProvider>
+      </MeshProvider>
+    </ErrorBoundary>
   )
 }
 
